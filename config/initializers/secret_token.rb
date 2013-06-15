d425aec00a0e801cc3dcf93e5a5abd661b42da2d
Amit Thawait <amit17thawait@gmail.com>
2013-06-15 08:02:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetsConnect::Application.config.secret_token = 'e7f14c15e97c3de6db3c0a965c64b5da0ddd3ca164b6c565a28d212b155e9f649fef5d763d07c5c17c43e9c7b081902f1d88828a8c9c60202e8bdd720f214e2b'
